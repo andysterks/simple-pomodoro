fec7f60f8d3e7097182ec6dbfdeb93516968fbd4
Andy Sterkowitz <apsterks@gmail.com>
2019-10-09 19:19:50 -0500

    Adds additional logic. Needs cleanup.

--- script.js
+++ script.js
@@ -1,3 +1,47 @@
+const TIMER_DEFAULT = 1500;
+const timerElement = document.querySelector('#timer-text');
+const resetButton = document.querySelector('#reset-btn');
+
+let currentTimer = TIMER_DEFAULT;
+
+let timerInterval;
+
 document.querySelector('#toggle-btn').onclick = (e) => {
-  
+  if (!timerInterval) {
+    timerInterval = setInterval(() => {
+      currentTimer -= 1;
+      updateDisplay();
+
+      if (currentTimer === 0) {
+        clearInterval(timerInterval);
+        // make beeping noise!
+      }
+    }, 1000);
+
+    e.target.innerHTML = 'Stop';
+    resetButton.disabled = true;
+
+  } else {
+    clearInterval(timerInterval);
+    timerInterval = undefined;
+
+    e.target.innerHTML = 'Start';
+    resetButton.disabled = false;
+  }
 };
+
+resetButton.onclick = (e) => {
+  currentTimer = TIMER_DEFAULT;
+  updateDisplay();
+}
+
+function getMinutes() {
+  var minutes = currentTimer % 60;
+  return minutes === 0 ? '00' : Math.floor(minutes);
+}
+
+function updateDisplay() {
+  timerElement.innerHTML = `${Math.floor(currentTimer / 60)}:${getMinutes()}`;
+}
+
+updateDisplay(`${Math.floor(currentTimer / 60)}:${getMinutes()}`);
