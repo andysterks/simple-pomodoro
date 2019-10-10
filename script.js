1e8159f87e7f352ab357be04bf511840fa6d5121
Andy Sterkowitz <apsterks@gmail.com>
2019-10-10 16:47:35 -0500

    Adds coloring to timer. Adds a status text.

--- script.js
+++ script.js
@@ -1,12 +1,15 @@
-const TIMER_DEFAULT = 1500;
-const toggleButton = document.querySelector('#toggle-btn');
-const timerElement = document.querySelector('#timer-text');
-const resetButton = document.querySelector('#reset-btn');
+const POMODORO_COUNT = 3;
+const BREAK_COUNT = 5;
+const toggleButton = document.querySelector("#toggle-btn");
+const statusElement = document.querySelector("#status-text");
+const timerElement = document.querySelector("#timer-text");
+const resetButton = document.querySelector("#reset-btn");
 
-let currentTimer = TIMER_DEFAULT;
+let currentTimer = POMODORO_COUNT;
+let timerIsActive = true;
 let timerInterval;
 
-toggleButton.onclick = (e) => {
+toggleButton.onclick = e => {
   if (!timerInterval) {
     timerInterval = setInterval(() => {
       currentTimer -= 1;
@@ -14,39 +17,52 @@ toggleButton.onclick = (e) => {
 
       if (currentTimer === 0) {
         clearInterval(timerInterval);
+        timerIsActive = false;
         // make beeping noise!
       }
     }, 1000);
 
-    e.target.innerHTML = 'Stop';
+    e.target.innerHTML = "Stop";
     resetButton.disabled = true;
-
   } else {
     clearInterval(timerInterval);
     timerInterval = undefined;
 
-    e.target.innerHTML = 'Start';
+    e.target.innerHTML = "Start";
     resetButton.disabled = false;
   }
 };
 
-resetButton.onclick = (e) => {
-  currentTimer = TIMER_DEFAULT;
+resetButton.onclick = e => {
+  if (currentTimer === 0) {
+    timerIsActive = !timerIsActive;
+    if (timerIsActive) {
+      statusElement.innerHTML = 'Go take a break!!!';
+    } else {
+      statusElement.innerHTML = 'Get to work!!!';      
+    }
+    statusClass = timerIsActive === true ? 'timer-active' : 'timer-inactive';
+    timerElement.className = statusClass;
+    statusElement.className = statusClass;
+  }
+  currentTimer = POMODORO_COUNT;
   updateDisplay();
-}
+};
 
 function getMinutes() {
-  const minutes = currentTimer / 60;
-  return minutes === 0 ? '00' : Math.floor(minutes);
+  let minutes = Math.floor(currentTimer / 60);
+  minutes = minutes < 10 ? '0' + minutes : minutes;
+  return minutes === 0 ? "00" : minutes;
 }
 
 function getSeconds() {
-  var seconds = currentTimer % 60;
-  return seconds === 0 ? '00' : Math.floor(seconds);
+  let seconds = Math.floor(currentTimer % 60);
+  seconds = seconds < 10 ? '0' + seconds : seconds;
+  return seconds === 0 ? "00" : seconds;
 }
 
 function updateDisplay() {
   timerElement.innerHTML = `${getMinutes()}:${getSeconds()}`;
 }
 
-updateDisplay(`${Math.floor(currentTimer / 60)}:${getSeconds()}`);
+updateDisplay();
